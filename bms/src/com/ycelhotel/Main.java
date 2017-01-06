cbd3a6b2aeaccd9fdd01f06cb5e51ebb666a4fa2
eyceky <eyceky@139.com>
2017-01-06 14:16:12 +0800

    完善了增删改查功能

--- bms/src/com/ycelhotel/Main.java
+++ bms/src/com/ycelhotel/Main.java
@@ -4,6 +4,5 @@ public class Main {
 	public static void main(String[] args){
 		Manager manager=new Manager();
 		manager.flow();
-		
 	}
 }
--- bms/src/com/ycelhotel/Manager.java
+++ bms/src/com/ycelhotel/Manager.java
@@ -7,14 +7,13 @@ import java.util.Scanner;
  *
  */
 public class Manager {
+	Scanner sc=new Scanner(System.in);
 	//用于存放Book类的数组
 	Book[] books=new Book[10]; 
 	/**
 	 *流程控制
 	 */
 	public void flow(){
-		Scanner sc=new Scanner(System.in);
-		System.out.println("进入flow方法");
 		while(true){
 			displayMenu();
 			int select=sc.nextInt();
@@ -55,7 +54,6 @@ public class Manager {
 		for(int i=0;i<menus.length;i++){
 			System.out.println(menus[i]);
 		}
-		System.out.println();
 	}
 	
 	/**
@@ -79,17 +77,20 @@ public class Manager {
 	 * 增加图书
 	 */
 	public void addBook(){
-		int count=0;
-		for(int i=0;i<books.length;i++){
-			if(books[i]!=null){
-				count++;
+		int inventory=0;
+		int empty=0;
+		//查看书库的库存
+		for(int index=0;index<books.length;index++){
+			if(books[index]!=null){
+				inventory++;
 			}
 		}
-		if(count==0){
+		empty=books.length-inventory;
+		if(empty==0){
 			System.out.println("书库已满");
 		}else{
-			System.out.println("还可以存放"+count+"本书");
-			Scanner sc=new Scanner(System.in);
+			System.out.println("书库中有"+inventory+"本书");
+			System.out.println("还可以存放"+empty+"本书");
 			Book book=new Book();
 			System.out.println("请输入书名：");
 			book.name=sc.next();
@@ -99,7 +100,12 @@ public class Manager {
 			book.publisher=sc.next();
 			System.out.println("请输入价格：");
 			book.price=sc.nextDouble();
-			sc.close();
+			for(int index=0;index<books.length;index++){
+				if(books[index]==null){
+					books[index]=book;
+					break;
+				}
+			}
 		}
 	}
 	
@@ -107,14 +113,33 @@ public class Manager {
 	 * 删除图书
 	 */
 	public void deleteBook(){
-		
+		System.out.println("请输入需要删除图书的书名：");
+		String bookName=sc.next();
+		for(int i=0;i<books.length;i++){
+			if(bookName.equals(books[i].name)){
+				books[i]=null;
+			}
+		}
 	}
 	
 	/**
 	 * 修改图书
 	 */
 	public void updateBook(){
-		
+		System.out.println("请输入需要修改图书的书名：");
+		String bookName=sc.next();
+		for(int i=0;i<books.length;i++){
+			if(bookName.equals(books[i].name)){
+				System.out.println("请输入书名：");
+				books[i].name=sc.next();
+				System.out.println("请输入作者：");
+				books[i].author=sc.next();
+				System.out.println("请输入出版社：");
+				books[i].publisher=sc.next();
+				System.out.println("请输入价格：");
+				books[i].price=sc.nextDouble();
+			}
+		}
 	}
 	
 }
